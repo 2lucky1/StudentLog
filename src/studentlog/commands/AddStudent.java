139aca6398c92a91649dd22a8d1039b430008d02
Nick <lanikolya1989@gmail.com>
2018-05-03 22:03:07 +0300

    Creating
    Created toolbar

---
+++ src/studentlog/commands/AddStudent.java
@@ -0,0 +1,13 @@
+package studentlog.commands;
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class AddStudent extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		System.out.println("Add menu");
+		return null;
+	}
+}
